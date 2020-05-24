c86b376cdf715d10700699f85a132b3070f823dd
Clement Mensah <clemensah10@gmail.com>
2020-05-24 07:49:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-  var x = []
+  var array = []
   for(var i = 0; i < musicians.length; i++){
     x.push(musicians[i] + ' plays '+ instruments[i])
   }
